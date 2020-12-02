2dd0f37986ac9ea1910ae3f28313075f14b4a87a
iamkuper <iamkuper@gmail.com>
2020-12-02 17:44:31 +0300

    Updates

---
+++ precache-manifest.de7360ffdf1f37eccb03f2d4ad790fe6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b3cbc39c8b031c0b974c1c1baf193af",
+    "url": "/vk-maryvish/index.html"
+  },
+  {
+    "revision": "4e03cc3bf7e4b68f79d7",
+    "url": "/vk-maryvish/static/css/2.cdee455b.chunk.css"
+  },
+  {
+    "revision": "d4d31b2bf9629b4dfb5b",
+    "url": "/vk-maryvish/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "4e03cc3bf7e4b68f79d7",
+    "url": "/vk-maryvish/static/js/2.b53a646b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/vk-maryvish/static/js/2.b53a646b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4d31b2bf9629b4dfb5b",
+    "url": "/vk-maryvish/static/js/main.2aeb22d5.chunk.js"
+  },
+  {
+    "revision": "db22c75f97b6def68915",
+    "url": "/vk-maryvish/static/js/runtime-main.e8524e32.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-maryvish/static/media/persik.4e1ec840.png"
+  }
+]);
